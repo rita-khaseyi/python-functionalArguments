c1b2526ec94c6f1c3d81152e617f56f7d7d4b976
rita khaseyi <ritakhaseyi@gmail.com>
2023-03-31 15:26:11 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(c,d):
+    subtraction=c-d
+    return subtraction
+
+def divide(e,f):
+    division=e/f
+    return division
+
+def multiply(g,h):
+    multiplication=g*h
+    return multiplication
+
+def remainder(i,j):
+    modulus=i%j
+    return modulus
+
